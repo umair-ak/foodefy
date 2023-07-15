202e48b58533f828edf8bf593f29b48ea4891f60
Umair <uak1911@gmail.com>
2023-07-15 23:12:47 +0530

    added all the static file to public folder, modified landing page

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express()
-app.use(bodyParser.urlencoder({extended:true}))
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
 app.get("/", function(req, res){
     res.sendFile(__dirname + "/index.html");
 });
