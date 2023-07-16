1f34c2b8a3f7c64887149c87e4924f42c6ace888
Umair <uak1911@gmail.com>
2023-07-16 13:23:35 +0530

    moved index and menu into view, made them ejs files and created partials to include in all the files

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express()
+const ejs = require("ejs");
+app.set("view engine","ejs");
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 app.get("/", function(req, res){
-    res.sendFile(__dirname + "/index.html");
+    res.render("index");
 });
 
 app.get("/menu",function(req,res){
-    res.sendFile(__dirname+"/menu.html");
+    res.render("menu");
 });
 
 app.listen(3000,function(){
